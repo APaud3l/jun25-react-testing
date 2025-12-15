973387fbc7c65e19a4e0a6945e0ad68f31d95f5d
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-12-15 19:29:06 +1100

    init: Initialised a sample React project and set up the test environment

---
+++ src/sample.test.js
@@ -0,0 +1,9 @@
+describe('Sanity check', () => { 
+    it('adds numbers correctly', () => {
+        // Arrange + Act
+        const result = 1 + 1;
+
+        // Assert
+        expect(result).toBe(2);
+    });
+ });
