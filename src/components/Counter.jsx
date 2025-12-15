2e6727fdd7bce56264e7ffe93609fc01a09bfa69
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-12-15 20:21:09 +1100

    test: Implemented dynamic testing by checking for value updates as increment and decrement buttons are clicked

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,6 +6,13 @@ function Counter(){
     const [count, setCount] = useState(0);
 
     // Add functions to increase and decrease the count
+    const handleIncrement = () => {
+        setCount((count) => count + 1);
+    };
+
+    const handleDecrement = () => {
+        setCount((count) => count - 1);
+    };
 
     // return some JSX
     return(
@@ -13,6 +20,8 @@ function Counter(){
             {/* Some elements go here */}
             <p>Count: {count}</p>
             {/* Buttons to be added later */}
+            <button onClick={handleDecrement}>Decrement</button>
+            <button onClick={handleIncrement}>Increment</button>
         </div>
     )
 }
--- src/tests/Counter.test.jsx
+++ src/tests/Counter.test.jsx
@@ -2,6 +2,7 @@
 // Necessary imports
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Counter from '../components/Counter';
 
 // test cases
@@ -14,4 +15,34 @@ test('shows the initial count', () => {
     // Assert: Check if the "0" is on the screen
     const countText = screen.getByText(/Count: 0/i);
     expect(countText).toBeInTheDocument();
-})
+});
+
+test('increments the count when "Increment" is clicked', async () => {
+    // Arrange: Render the Counter component
+    render(<Counter />)
+
+    const user = userEvent.setup();
+
+    // Act: Find the "Increment" button
+    const incrementButton = screen.getByRole('button', { name: /increment/i });
+    // Click it
+    await user.click(incrementButton);
+
+    // Assert: Check if "Count: 1" is in the Document
+    expect(screen.getByText(/Count: 1/i)).toBeInTheDocument();
+});
+
+test('decrements the count when "Decrement" is clicked', async () => {
+    // Arrange: Render the Counter component
+    render(<Counter />)
+
+    const user = userEvent.setup();
+
+    // Act: Find the "Decrement" button
+    const decrementButton = screen.getByRole('button', { name: /decrement/i });
+    // Click it
+    await user.click(decrementButton);
+
+    // Assert: Check if "Count: 1" is in the Document
+    expect(screen.getByText(/Count: -1/i)).toBeInTheDocument();
+});
